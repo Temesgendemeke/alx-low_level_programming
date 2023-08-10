629c9b70c721bb71830d4eb44d4e89bf448a2f9a
Temesgen Dora <tdemeke36@gmail.com>
2023-08-10 12:50:21 -0700

    strcpy

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+#include <string.h>
+
+int main(){
+    char alx[52];
+
+
+    strcpy(alx,"Programming is like building a multilingual puzzle\n");
+
+    puts(alx);
+
+	return(0);
+}
