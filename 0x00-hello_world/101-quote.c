57b4b266617fc71a855ba043ae86d12dbcdbe176
Temesgen Dora <tdemeke36@gmail.com>
2023-08-11 00:49:43 -0700

    assign

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Description: 'prints string with newline'
+  * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (0);
+}
