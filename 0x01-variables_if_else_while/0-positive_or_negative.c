eb23b735aacb6c17b210296b27a97458a2254fe1
Temesgen Dora <tdemeke36@gmail.com>
2023-08-11 11:54:49 -0700

    f

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+   * main - Entry point
+   *
+   * Description: 'checke if a number is negative postibe or zero'
+   * Return: Always 0 (Success)
+ */
 int main(void)
 {
 if (n > 0)
