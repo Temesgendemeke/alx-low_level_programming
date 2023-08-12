a95d69065d7c92a999c03ca992b8802ad22fc32a
Temesgen Dora <tdemeke36@gmail.com>
2023-08-12 14:02:53 -0700

    hard

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,31 +1,29 @@
- #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <stdio.h>
 
 /**
- * main - Prints all single digit numbers of base 10
- * Return: Always 0 (success)
+ * main - Prints all possible combinations of two different digits,
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
-        int a, b;
+int a, b;
 
-        for (a = 0; a <= 98; a++)
-        {
-        for (b = a + 1; b <= 99; b++)
-        {
-        putchar((a / 10) + '0');
-        putchar((a % 10) + '0');
-        putchar(' ');
-        putchar((b / 10) + '0');
-        putchar((b % 10) + '0');
-        if (a == 98 && b == 99)
-        continue;
-        putchar(',');
-        putchar(' ');
-        }
-        }
-        putchar('\n');
-        return (0);
+	for (a = 0; a < 9; a++)
+	{
+		for (b = a + 1; b < 10; b++)
+		{
+			putchar((a % 10) + '0');
+			putchar((b % 10) + '0');
+
+			if (a == 8 && b == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
 }
