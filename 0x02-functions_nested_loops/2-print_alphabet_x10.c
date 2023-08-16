d598aa5a2caa94bcd35f9f8e86862fe5e6373b6d
Temesgen Dora <tdemeke36@gmail.com>
2023-08-16 02:18:42 -0700

    DG|

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,8 @@
 #include "main.h"
 /**
-  * main - Entry point
-  *
-  * Description: 'prints string with newline'
-  * Return: Always 0 (Success)
-*/
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ */
 void print_alphabet_x10(void)
 {
 int n = 0;
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+/** 
+ * _isalpha - check if char is lowercase
+ * @c : is the char to be checked
+ * Return: 1 if char is lowercase, otherwise 0.
+ */
+
+int _isalpha(int c)
+{
+
+if (islower('c') > 0)
+{
+_putchar(49);
+}
+else
+{
+_putchar(48);
+}
+
+}
