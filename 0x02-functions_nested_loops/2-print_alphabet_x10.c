36b3459743ff96a8cce8e988ddfdda8b020c08e1
Temesgen Dora <tdemeke36@gmail.com>
2023-08-16 01:33:06 -0700

    DFG

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * main - check the code.
- *
+ * Description: 'prints string with newline'
  * Return: Always 0.
  */
 void print_alphabet_x10(void)
 {
 int n = 0;
 
-while (n <= 10)
+while (n < 10)
 {
 
 char h;
