a794e2644006d431b2ab4a9ee2bc2d8666f88e93
Temesgen Dora <tdemeke36@gmail.com>
2023-08-16 01:35:40 -0700

    FHFH

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
- * main - check the code.
- * Description: 'prints string with newline'
- * Return: Always 0.
- */
+  * main - Entry point
+  *
+  * Description: 'prints string with newline'
+  * Return: Always 0 (Success)
+*/
 void print_alphabet_x10(void)
 {
 int n = 0;
