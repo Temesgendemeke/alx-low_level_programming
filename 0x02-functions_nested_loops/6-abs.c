4c0e5b48ee998b0d93358cecedad5ced6233cb85
Temesgen Dora <tdemeke36@gmail.com>
2023-08-16 03:17:38 -0700

    fd

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - computes the absolute value of an integer
+ * @int: - is number to be checked
+ * Return: return absoulte value.
+ */
+int _abs(int)
+{
+if (_abs() >= 0)
+{
+_putchar(_abs());
+}
+else if (_abs() < 0)
+{
+_ptchar(-(_abs()));
+}
+
+}
