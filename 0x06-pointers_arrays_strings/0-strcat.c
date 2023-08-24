6d918b45ecc9e5c8d28b5189ecd9819c2a780d53
Temesgen Dora <tdemeke36@gmail.com>
2023-08-24 05:04:52 -0700

    hello

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,20 +5,16 @@
  * @src : source
  * Return: the length of the string
  */
-char *_strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src)
 {
-char *ptr = dest;
-while (*ptr != '\0')
-{
+char* ptr = dest;
+while (*ptr != '\0') {
 ptr++;
-
 }
-while (*src != '\0')
-{
+while (*src != '\0') {
 *ptr = *src;
 ptr++;
 src++;
 }
 *ptr = '\0';
-return (dest);
 }
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+
+char new(char *dest)
+{
+
+char *ptr = dest;
+
+while (*ptr = '\0')
+
+
+
+return dest;
+}
+
+int main()
+{
+
+char h1[30] = "sealm";
+
+
+new(h1);
+
+
+return 0;
+}
