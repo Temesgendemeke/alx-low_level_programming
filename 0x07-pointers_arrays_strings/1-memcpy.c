5baca69e5b04568b18d400677c7212b598c74d45
Temesgen Dora <tdemeke36@gmail.com>
2023-08-28 04:52:45 -0700

    quite

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,13 +8,14 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-int i = 0;
-while (i < n)
-{
-dest[i] = src[i];
-i++;
-n--;
-}
+	int r = 0;
+	int i = n;
 
-return (dest);
+	for (; r < i; r++)
+	{
+		dest[r] = src[r];
+		n--;
+	}
+	return (dest);
+}
 }
