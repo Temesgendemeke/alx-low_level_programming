8e1e12d13c37c1ce7285c5a9afe419c8328e8bfe
Temesgen Dora <tdemeke36@gmail.com>
2023-08-28 11:15:34 -0700

    101-crackme_password

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - a function that sets the value of a pointer to a char.
+ * @s: value of a pointer
+ * @to: pointer the value to be store
+ * Return: Always 0.
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
