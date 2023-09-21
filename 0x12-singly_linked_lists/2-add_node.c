ed19460e2115a08c2f462c0ade2bb2ce1192b82a
temesgendemeke <temesgen100k@gmail.com>
2023-09-21 14:09:04 -0700

    fixed betty and added readme

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * add_node - a function that adds a new node at the beginning of a list_t list.
+ * add_node - adds a new node at the beginning of a list_t list.
  * @head :head ptr
  * @str : string ptr
  * Return: head
