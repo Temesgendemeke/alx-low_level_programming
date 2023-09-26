9ece39be79013924ae6ee4ea5cff47443c7ff9db
temesgendemeke <temesgen100k@gmail.com>
2023-09-26 11:26:04 -0700

    task 9

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -5,26 +5,28 @@
  * @index: nth element
  * Return: return 1 -1 if it failed
  */
-int delete_nodeint_at_index(listint_t **head, unsigned int index) {
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
 	listint_t *current = *head;
 	listint_t *previous = NULL;
-	if (*head == NULL )
+
+	if (*head == NULL)
 	{
 		return (-1);
 	}
-	if (index == 0) 
+	if (index == 0)
 	{
 		*head = current->next;
 		free(current);
 		return (1);
 	}
-	while (current != NULL && index > 0) 
+	while (current != NULL && index > 0)
 	{
 		previous = current;
 		current = current->next;
 		index--;
 	}
-	if (current == NULL) 
+	if (current == NULL)
 	{
 		return (-1);
 	}
