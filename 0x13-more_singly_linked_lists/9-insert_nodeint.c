150d8a70dc74d8565bfc49446445c1cd7732b424
temesgendemeke <temesgen100k@gmail.com>
2023-09-26 10:33:38 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,33 +8,31 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *newNode = malloc(sizeof(listint_t));
-listint_t *current = *head;
-if (newNode == NULL)
-{
-return (NULL);
-}
+	listint_t *newNode = malloc(sizeof(listint_t));
+	listint_t *current = *head;
 
-newNode->n = n;
-newNode->next = NULL;
-if (idx == 0) 
-{
-    newNode->next = *head;
-    *head = newNode;
-    return (newNode);
-}
-
-while (current != NULL && idx > 1)
-{
-current = current->next;
-idx--;
-}
-if (current == NULL || idx > 1) 
-{
-    return NULL;
-}
-
-newNode->next = current->next;
-current->next = newNode;
-return (newNode);
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+	newNode->n = n;
+	newNode->next = NULL;
+	if (idx == 0)
+	{
+		newNode->next = *head;
+		*head = newNode;
+		return (newNode);
+	}
+	while (current != NULL && idx > 1)
+	{
+		current = current->next;
+		idx--;
+	}
+	if (current == NULL || idx > 1)
+	{
+		return (NULL);
+	}
+	newNode->next = current->next;
+	current->next = newNode;
+	return (newNode);
 }
