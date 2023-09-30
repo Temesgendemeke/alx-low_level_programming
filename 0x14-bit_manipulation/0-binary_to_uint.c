86de76fc5a988733fbb691bcbbc065c15edaf683
temesgendemeke <temesgen100k@gmail.com>
2023-09-30 08:35:45 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,23 +6,22 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int conv = 0;
-    unsigned int num = 1;
-    int i;
+	int conv = 0;
+	int num = 1;
+	int i;
 
-    if (b == NULL)
-        return (0);
+	if (b == NULL)
+		return (0);
 
-    for (i = strlen(b) - 1; i >= 0; i--)
-    {
-        if (b[i] != '1' || b[i] != '0')
-            return 0;
-
-        else if (b[i] == '1') 
+	for (i = strlen(b) - 1; i >= 0; i--)
 	{
-            conv += num;
-        }
-        num *= 2;
-    }
-    return conv;
+		if (b[i] != '1' && b[i] != '0')
+			return (0);
+		else if (b[i] == '1')
+		{
+			conv += num;
+		}
+		num *= 2;
+	}
+	return (conv);
 }
