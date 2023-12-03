8955b829d6c4d390674774140b1ee2812dcdd51d
Temesgen dora <tdemeke36@gmail.com>
2023-12-03 00:35:53 -0800

    task fixed

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,11 +1,17 @@
 #include "hash_tables.h"
+/**
+  * hash_table_create - creates hash table
+  * @size: size of hash table to be created
+  * Return: table otherwise NULL
+*/
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
-    if (table == NULL)
-    {
-        return NULL;
-    }
-    table->size = size;
-    return table;
+	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+	{
+		return (NULL);
+	}
+	table->size = size;
+	return (table);
 }
